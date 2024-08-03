ccdf92ef75f32bceb4644f0104f5ce393b40b241
Heath <duweihengmr@163.com>
2024-08-03 23:40:03 +0800

    tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: ["index.html", "./src/**/*.{html,js,ts,jsx,tsx,vue}"],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
+
