326c87d0144b8015dd0d4cbdababac9364aad2e7
Heath <duweihengmr@163.com>
2024-08-04 22:29:29 +0800

    CustomSearchInput

---
+++ src/components/CustomSearchInput.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="custom-search-input" :style="props.style" :class="props.class">
+    <input
+        v-model="value"
+        :placeholder="props.placeholder"
+        @keydown.enter="$emit('on-search')"
+    >
+    <div @click="$emit('on-search')" class="cursor-pointer">
+      <i class="iconfont icon-sousuo mr-[10px] text-[var(--primary-color)]" style="font-size: 16px"/>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  placeholder: String,
+  style: String,
+  class: String,
+})
+const value = defineModel('value')
+defineEmits(["on-search"])
+</script>
+
+<style lang="less" scoped>
+.custom-search-input {
+  margin-top: 18px;
+  margin-bottom: 18px;
+  height: 38px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #E3ECFF;
+  border-radius: 10px;
+
+  input {
+    font-size: 14px;
+    font-weight: 600;
+    margin: 0 0 0 10px;
+    border: none;
+    outline: none;
+    width: 100%;
+    background-color: transparent;
+  }
+
+  input::placeholder {
+    color: #4C9BFF
+  }
+
+}
+</style>
