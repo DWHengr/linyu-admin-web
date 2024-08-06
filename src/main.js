f35480a311be8fa0348a290df0b7503ac68a2a2c
Heath <duweihengmr@163.com>
2024-08-06 21:51:53 +0800

    storePlugin

--- src/main.js
+++ src/main.js
@@ -4,9 +4,11 @@ import './assets/iconfont.css'
 import {createApp} from 'vue'
 import App from './App.vue'
 import router from './router'
+import {storePlugin} from "@/utils/StorePlugin.js";
 
 const app = createApp(App)
 
+app.use(storePlugin)
 app.use(router)
 
 app.mount('#app')
