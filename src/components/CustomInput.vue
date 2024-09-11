887264e764866a0a87202f13c14824f1ec3cc016
Heath <duweihengmr@163.com>
2024-09-11 20:30:45 +0800

    CustomInput

---
+++ src/components/CustomInput.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <input :type="props.type" class="custom-input" :placeholder="props.placeholder" v-model="value"/>
+  </div>
+</template>
+
+<script setup>
+import {defineProps, defineModel} from "vue";
+
+const props = defineProps({
+  placeholder: String,
+  type: String
+})
+const value = defineModel('value')
+</script>
+
+<style scoped lang="less">
+.custom-input {
+  padding: 0 10px;
+  height: 50px;
+  width: 100%;
+  border-radius: 5px;
+  background-color: #F0F0F0;
+  font-size: 18px;
+  color: #8D8D8D;
+  margin-top: 30px;
+  display: flex;
+  outline: none;
+}
+</style>
