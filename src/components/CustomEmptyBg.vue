4053fae101b20f23a2f79c9cb31661bb15d4b2c2
Heath <duweihengmr@163.com>
2024-10-18 20:20:42 +0800

    CustomEmptyBg

---
+++ src/components/CustomEmptyBg.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="flex w-full h-full items-center justify-center bg-[#F9FBFF]">
+    <img style="height: 120px" src="/bg.png" alt="empty"/>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+</style>
