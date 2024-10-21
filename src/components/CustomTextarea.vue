9f66d5fcf9c49ec76f9b93f8f04bb86f1dc5e6ec
Heath <duweihengmr@163.com>
2024-10-21 21:17:56 +0800

    CustomTextarea

---
+++ src/components/CustomTextarea.vue
@@ -0,0 +1,27 @@
+<template>
+  <textarea class="textarea" v-model="value" :placeholder="placeholder"></textarea>
+</template>
+<script setup>
+import {defineProps, defineModel} from 'vue';
+
+const props = defineProps({
+  placeholder: String
+})
+const value = defineModel('value')
+</script>
+<style lang="less" scoped>
+.textarea {
+  border-radius: 2px;
+  background-color: #F9FBFF;
+  resize: none;
+  width: 100%;
+  min-height: 100%;
+  outline: none;
+  padding: 0 10px;
+  font-size: 14px;
+
+  &::placeholder {
+    color: #7F7F7F;
+  }
+}
+</style>
